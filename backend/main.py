047fdc24d0adacde4f061e6e19fe1c4797ffc0fb
atharvkanchan <kanchanatharvl614@gmail.com>
2026-04-22 12:06:48 +0530

    Add files via upload

--- backend/main.py
+++ backend/main.py
@@ -1,1 +1,13 @@
 
+from fastapi import FastAPI
+from routes.expenses import router as expense_router
+from auth.auth_routes import router as auth_router
+
+app = FastAPI(title="FinSight AI Pro")
+
+app.include_router(expense_router)
+app.include_router(auth_router)
+
+@app.get("/")
+def home():
+    return {"message": "FinSight AI Pro Running"}
